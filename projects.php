dee523e06398ffc8a87770011902ef59217b733d
James7304 <jamesmccorkindale0@gmail.com>
2022-06-25 18:59:04 +0100

    Added jamesmccorkindale.co.uk to project folio

--- projects.php
+++ projects.php
@@ -93,6 +93,32 @@
             <h2 class="p-3">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-signpost-2" viewBox="0 0 16 16">
               <path d="M7 1.414V2H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h5v1H2.5a1 1 0 0 0-.8.4L.725 8.7a.5.5 0 0 0 0 .6l.975 1.3a1 1 0 0 0 .8.4H7v5h2v-5h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H9V6h4.5a1 1 0 0 0 .8-.4l.975-1.3a.5.5 0 0 0 0-.6L14.3 2.4a1 1 0 0 0-.8-.4H9v-.586a1 1 0 0 0-2 0zM13.5 3l.75 1-.75 1H2V3h11.5zm.5 5v2H2.5l-.75-1 .75-1H14z"/>
+            </svg>jamesmccorkindale.co.uk</h2>
+
+            <div class="row p-3" width="100%">
+                <i>Initial deployment/release: 26/05/2022</i>
+                <div class="col-8 d-flex justify-content-left">
+                    I created a personal website to showcase my projects, and advertise for my web design and tutoring businesses.<br>
+                    A contact form is also available on my website so people can get in touch with me.
+                </div>
+                <div class="col-4 d-flex justify-content-end">
+                    <i class="mt-auto">
+                        GitHub Repo: <a href="https://github.com/James7304/jamesmccorkindale.co.uk">jamesmccorkindale.co.uk</a><br>
+                    </i>
+                </div>
+            </div>
+
+        </div>
+    
+    </div>
+
+    <div class="row align-items-center">
+
+        <div class="col bg-light" id="project-container">
+
+            <h2 class="p-3">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-signpost-2" viewBox="0 0 16 16">
+              <path d="M7 1.414V2H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h5v1H2.5a1 1 0 0 0-.8.4L.725 8.7a.5.5 0 0 0 0 .6l.975 1.3a1 1 0 0 0 .8.4H7v5h2v-5h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H9V6h4.5a1 1 0 0 0 .8-.4l.975-1.3a.5.5 0 0 0 0-.6L14.3 2.4a1 1 0 0 0-.8-.4H9v-.586a1 1 0 0 0-2 0zM13.5 3l.75 1-.75 1H2V3h11.5zm.5 5v2H2.5l-.75-1 .75-1H14z"/>
             </svg>DesignArk</h2>
 
             <div class="row p-3" width="100%">
