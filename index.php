90acb6a772c01e564e7d14b9d85abdad1b6c4d8b
James McCorkindale <77486262+James7304@users.noreply.github.com>
2022-11-02 13:32:41 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>James McCorkindale</title>
+    <title>James</title>
     <link rel="icon" href="./img/icon.png">
     
     <link rel="stylesheet" href="styles/media.css">
